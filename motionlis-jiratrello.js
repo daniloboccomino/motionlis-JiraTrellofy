403cb1312225e4747a4fd040c0b4eeea6aa5e262
Danilo Boccomino <57117114+daniloboccomino@users.noreply.github.com>
2025-02-20 14:09:40 -0300

    Update motionlis-jiratrello.js

--- motionlis-jiratrello.js
+++ motionlis-jiratrello.js
@@ -19,52 +19,97 @@ JiraTrellofy = (options = {}) => {
     }
 
     /**
-     * Substitui o nome completo do usuário pelo seu nickname customizado.
+     * Ícones utilizados para labels específicas caso a opção `disableLabelIcon` esteja desabilitada.
+     * Podem ser sobrescritos por `LABELS_ICON = {}` definidos por cada usuário.
+     * Para incluir novos Ícones, é necessario informar:
+     *  NOME_LABEL: {
+            icon: 'nome_icone',
+            animate: true,
+            background:'red',
+            color:'white',
+        }
+     *
+     * https://fonts.google.com/icons
      */
-    const replaceNickname = (link) => {
-        const nickname = options.assigneeNicks?.[Object.keys(options.assigneeNicks).filter(key => link.innerHTML.trim().startsWith(key))]
-        if (!nickname) return
-        link.innerHTML = nickname
+    const ICONS = {
+        URGENTE: {
+            icon: 'e911_emergency',
+            animate: true,
+            background:'red',
+            color:'white',
+        },
+        ATENCAO: {
+            icon: 'warning',
+            background:'gold',
+        },
+        DEPENDENCIA: {
+            icon: 'link',
+        },
+        PASTELARIA: {
+            icon: 'lunch_dining',
+        },
+        TESTES_AUTOMATICOS: {
+            icon: 'terminal',
+        }
     }
 
     /**
-     * Coloca o avatar do usuário no corpo de seu link. Caso a opção 'avatarOnly' estiver habilitada,
-     * sera exibido apenas o avatar tendo o nome do usuário omitido.
+     * Substitui o nome completo do usuário pelo seu nickname customizado.
+     * Caso a opção 'avatarOnly' estiver habilitada, sera exibido apenas o avatar tendo o nome do usuário omitido.
      */
-    const putAvatar = (link) => {
-        let username = link.href?.split('name=')[1]
-        if (!username) return
-        username = options.fixUsernameLDAP?.[username] || username
-        const avatar = document.createElement('img')
-        avatar.src = `https://jira.touchtec.com.br/secure/useravatar?ownerId=${username}`
-        avatar.width = avatar.height = 30
-        if (options.avatarOnly) {
-            link.innerHTML = null
+    const replaceNickname = (link) => {
+        if ((options.withAvatar??true) && (options.avatarOnly??true)) {
+            link.closest('.tinylink')?.parentElement.classList.add('min-width')
+            return ''
         }
-        link.prepend(avatar)
+        const nickname = options.assigneeNicks?.[
+            Object.keys(options.assigneeNicks).filter((key) => link.innerText.trim().startsWith(key))
+        ]
+        if (nickname) return nickname
+        else if (link.innerText.indexOf(' ') > 0) return link.innerText.substr(0, link.innerText.indexOf(' '))
     }
 
-  /**
-   * Adiciona o avatar e o apelido dos usuários para cada link de usuário vinculado a issue.
-   */
+    /**
+     * Coloca o avatar do usuário no corpo de seu link.
+     */
+    const putAvatar = (link) => {
+        let username = link.rel
+        if (!username) return
+        username = options.fixUsernameLDAP?.[username] || username
+        link.prepend(Object.assign(
+            document.createElement('img'),
+            { height: 30, width: 30, src: `https://jira.touchtec.com.br/secure/useravatar?ownerId=${username}` }
+        ))
+    }
+
+    /**
+     * Adiciona o avatar e o apelido dos usuários para cada link de usuário vinculado a issue.
+     */
     const formatLinkUsers = (row) => {
         const linksUser = row.querySelectorAll(`a.${CSS.LINK_USER}`)
         if (linksUser.length === 0) return
-        linksUser.forEach(link => {
-            replaceNickname(link)
-            if (options.withAvatar || options.avatarOnly) {
-                putAvatar(link)
-            }
+        linksUser.forEach((link) => {
+            link.innerText = replaceNickname(link)
+            if (options.withAvatar??true) putAvatar(link)
         })
     }
 
     /**
-     * Remove uma quebra de linha (<br>) que são inseridas (intermitente) na coluna dos commits do gerrit.
+     * Remove uma quebra de linha `<br>` que são inseridas (intermitente) na coluna dos commits do gerrit.
      */
     const removeFirstBRFromCommitsArea = (row) => {
         const commitsCell = row.querySelector(`.${CSS.GERRIT_COMMITS}`)
         if (!commitsCell) return
         commitsCell.querySelector('br')?.remove()
+    }
+
+    const fixGerritlinksTextView = (gerritlinks) => {
+        let p = gerritlinks.querySelector('p');
+        if (p) {
+            let br = p.querySelectorAll('br');
+            if (br.length) br.forEach((br) => br.remove());
+            p.querySelectorAll('a').forEach((a) => a.after(document.createElement('br')))
+        }
     }
 
     /**
@@ -88,29 +133,29 @@ JiraTrellofy = (options = {}) => {
         }
         cellN3.setAttribute('colspan', colspan)
     }
-    
+
     /**
-     * Adiciona icones para labels especificas
+     * Troca labels especificas para Ícones
      */
-    const addLabelIcon = (row, labels) => {
-        labels.querySelectorAll('ul.labels li').forEach(label => {
-            let labelText = label.innerText.trim()
-            let icon = options.labelIcon?.[labelText]
-            if (icon) {
-                const link = label.querySelector('a')
-                link.classList.add('label-'+labelText)
-                link.innerHTML = icon.svg
-                link.style = 'background: ' + icon.cor
-            }
+    const changeLabelIcon = (row, labels) => {
+        labels.querySelectorAll('ul.labels li').forEach((li) => {
+            let labelText = li.innerText.trim()
+            let label = options.labelsIcon?.[labelText]
+            let padrao = ICONS[labelText]
+            const a = li.querySelector('a')
+            if (label) setLabelIcon(a, labelText, label)
+            else if (padrao) setLabelIcon(a, labelText, padrao)
         })
     }
-    
-    const removeProjectName = (gerritlinks) => {
-        let p = gerritlinks.querySelector('p')
-        if (p) {
-            let br = p.querySelectorAll('br')
-            if (br.length) br.forEach(br => br.remove())
+
+    const setLabelIcon = (a, labelText, icon) => {
+        if (icon.icon) {
+            a.className += ` label-${labelText} ${icon.animate ? 'animate' : ''}`
+            a.innerHTML = `<span class="material-symbols-rounded">${icon.icon}</span>`
         }
+        a.style.background = icon.background
+        a.style.color = icon.color
+        a.title = icon.title ? icon.title : a.title
     }
 
     /**
@@ -126,16 +171,12 @@ JiraTrellofy = (options = {}) => {
      */
     const postCompactRow = (row, subRow) => {
         removeOthersUsersWhenN3(subRow)
-        
+
         let labels = subRow.querySelector('td.labels')
-        if (labels) {
-            addLabelIcon(row, labels)
-        }
-        
+        if (labels && !options.disableLabelIcon) changeLabelIcon(row, labels)
+
         let gerritlinks = subRow.querySelector(`.${CSS.GERRIT_COMMITS}`)
-        if (gerritlinks) {
-            removeProjectName(gerritlinks)
-        }
+        if (gerritlinks) fixGerritlinksTextView(gerritlinks)
     }
 
     /**
@@ -154,11 +195,11 @@ JiraTrellofy = (options = {}) => {
         preCompactRow(row)
 
         // primeiras celulas posterior ao número da issue ocupando o espaço de duas linhas
-        for (let firstCell = cells.shift(); cells.length > 1; firstCell = cells.shift()) {
+        for (let firstCell = cells.shift() ; cells.length > 1 ; firstCell = cells.shift()) {
             fillTwoRows(firstCell)
             if (firstCell.className.indexOf(CSS.ISSUE_KEY) >= 0) break
         }
-        if (cells.length <= (MAX_CELL_COMPACT - 1)) return
+        if (cells.length <= MAX_CELL_COMPACT - 1) return
 
         // ultima celula (acoes) ocupando o espaço de duas linhas
         const lastCell = cells[cells.length - 1]
@@ -171,9 +212,7 @@ JiraTrellofy = (options = {}) => {
         const subRow = document.createElement('tr')
         subRow.classList.add('compactRow')
         row.after(subRow)
-        cells.forEach((cell) => {
-            subRow.append(cell)
-        })
+        cells.forEach((cell) => subRow.append(cell))
 
         postCompactRow(row, subRow)
     }
@@ -194,21 +233,36 @@ JiraTrellofy = (options = {}) => {
         cells.forEach((row) => compactRow(row))
 
         const gadgets = content.querySelectorAll('#dashboard-content > .gadget')
-        gadgets.forEach(gadget => reduceTitle(gadget))
+        gadgets.forEach((gadget) => reduceTitle(gadget))
     }
 
     window.addEventListener('load', (event) => {
+        if (!options.disableLabelIcon) {
+            document.head.append(Object.assign(
+                document.createElement('link'),
+                { rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,500,0,0" }
+            ))
+        }
+        run()
         const jira = document.getElementById('jira')
-        const observer = new MutationObserver(
-            (mutationList, observer) => {
-                window.clearTimeout(jira.timeoutRunner)
-                // timeout com override para não ficar disparando toda hora
-                // quando multiplos elementos estão sendo carregados na DOM
-                jira.timeoutRunner = setTimeout(() => run(), 100)
-            }
-        )
+        const observer = new MutationObserver((mutationList, observer) => {
+            window.clearTimeout(jira.timeoutRunner)
+            // timeout com override para não ficar disparando toda hora
+            // quando multiplos elementos estão sendo carregados na DOM
+            jira.timeoutRunner = setTimeout(() => run(), 100)
+        })
+
         // verifica se algum elemento é inserido no corpo da div principal do Jira
-        observer.observe(jira, { attributes: false, childList: true, subtree: true });
+        observer.observe(jira, { attributes: false, childList: true, subtree: true })
     })
-    
+
+    window.addEventListener('keypress', (event) => {
+        if (event.key === 'q') {
+            var currentUser = document.getElementById('header').querySelector('nav.aui-header li#user-options > a').getAttribute('data-username')
+            document.getElementById('content').querySelectorAll(`.issue-table .compactRow:not(.issuerow):not(:has(.tinylink > a[rel="${currentUser}"]))`).forEach(item => {
+                item.classList?.toggle('hide-shortchut')
+                item.previousSibling?.classList?.toggle('hide-shortchut')
+            })
+        }
+    })
 }
