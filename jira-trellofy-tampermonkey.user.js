5305619920e74457e45f3aea72b50d1c084c59cb
Danilo Boccomino <57117114+daniloboccomino@users.noreply.github.com>
2026-09-11 19:22:09 -0300

    Add files via upload

---
+++ jira-trellofy-tampermonkey.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         JiraTrellofy Loader
+// @namespace    touchtec
+// @version      1.0.0
+// @description  Bootstrap que injeta o JiraTrellofy (js/css) a partir dos anexos da MOTAUTO-24
+// @match        https://jira.touchtec.com.br/secure/Dashboard.jspa?selectPageId=53772
+// @grant        none
+// @updateURL    https://github.com/daniloboccomino/motionlis-JiraTrellofy/blob/master/jira-trellofy-tampermonkey.user.js
+// @downloadURL  https://github.com/daniloboccomino/motionlis-JiraTrellofy/blob/master/jira-trellofy-tampermonkey.user.js
+// ==/UserScript==
+
+(async () => {
+    const findAttachment = (doc, extension) => {
+        return doc.querySelector(`dt.attachment-title > a[href$='jira-trellofy.${extension}']`).href
+    }
+
+    const page = await fetch('https://jira.touchtec.com.br/browse/MOTAUTO-24')
+    const doc = new DOMParser().parseFromString(await page.text(), 'text/html')
+
+    const [css, js] = await Promise.all([
+        fetch(findAttachment(doc, 'css')).then((r) => r.text()),
+        fetch(findAttachment(doc, 'js')).then((r) => r.text()),
+    ])
+
+    const cleanCSS = (css) => {
+        return css
+            .replace(/\/\*[\s\S]*?\*\//g, '')   // Remove comentários
+            .replace(/\s+/g, ' ')               // Substitui múltiplos espaços por um
+            .replace(/\s*([{}:;,])\s*/g, '$1')  // Remove espaços ao redor de pontuações
+            .trim()                             // Remove espaços nas pontas
+    }
+
+    document.head.append(
+        Object.assign(document.createElement('style'), { innerText: cleanCSS(css) })
+    )
+
+    document.body.append(
+        Object.assign(document.createElement('script'), { text: js })
+    )
+
+    JiraTrellofy({
+        /* Configurações personalizadas para alterar o comportamento padrão do JiraTrellofy() */
+    })
+})()
