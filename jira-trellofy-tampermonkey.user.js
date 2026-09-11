122962cae7ef944d8fe8911bc040ec18f6c5998d
Danilo Boccomino <57117114+daniloboccomino@users.noreply.github.com>
2026-09-11 20:48:13 -0300

    Update jira-trellofy-tampermonkey.user.js

--- jira-trellofy-tampermonkey.user.js
+++ jira-trellofy-tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JiraTrellofy Loader
 // @namespace    touchtec
-// @version      1.0.1
+// @version      1.0.2
 // @description  Bootstrap que injeta o JiraTrellofy (js/css) a partir dos anexos da MOTAUTO-24
 // @match        https://jira.touchtec.com.br/secure/Dashboard.jspa?selectPageId=53772
 // @grant        none
@@ -22,10 +22,16 @@
         fetch(findAttachment(doc, 'js')).then((r) => r.text()),
     ])
 
-    
+    const cleanCSS = (css) => {
+        return css
+            .replace(/\/\*[\s\S]*?\*\//g, '')   // Remove comentários
+            .replace(/\s+/g, ' ')               // Substitui múltiplos espaços por um
+            .replace(/\s*([{}:;,])\s*/g, '$1')  // Remove espaços ao redor de pontuações
+            .trim()                             // Remove espaços nas pontas
+    }
 
-    document.head.prepend(
-        Object.assign(document.createElement('style'), { innerText: css })
+    [...temp1.querySelectorAll('link[type="text/css"][rel="stylesheet"]')].at(-1).after(
+        Object.assign(document.createElement('style'), { innerText: cleanCSS(css) })
     )
 
     document.body.append(
